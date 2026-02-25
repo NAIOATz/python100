8c97f7861ee3a7c4548f35e8b1514844933fddd1
YAIMAKMAK <surapoj6029@gmail.com>
2026-02-25 23:43:53 +0700

    add AKA Genearator

---
+++ AKA_GENERATOR/main.py
@@ -0,0 +1,4 @@
+print("welcome to AKA Genearator!")
+last_meal = input("What was your last meal? : ")
+last_drink = input("What was the last drink you had? : ")
+print(f'Your AKA could be "{last_meal} {last_drink}"')

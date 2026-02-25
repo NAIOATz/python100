856bf929e8344d537b9d620242cb6bf5df6924e1
YAIMAKMAK <surapoj6029@gmail.com>
2026-02-25 23:54:26 +0700

    BMI calculator

---
+++ BMI_calculator/main.py
@@ -0,0 +1,12 @@
+# BMI Calculator
+# The body mass index (BMI) is a measure used in medicine to see if someone is underweight or overweight. This is the formula used to calculate it:
+
+# bmi is equal to the person's weight divided by the person's height squared.
+# bmi = weight / (height ** 2)
+
+weight = int(input("Input your weight(kg.): "))
+height = int(input("Type your height(cm.): "))
+
+bmi = weight / (height ** 2)
+
+print(f'This is your BMI : {bmi}')
